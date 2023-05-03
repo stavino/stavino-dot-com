0a1f34b9864ebd5d24297f12b55e2c4b509e1ee2
stavino <89891417+stavino@users.noreply.github.com>
2023-05-03 16:40:49 -0400

    hopefully

--- src/App.js
+++ src/App.js
@@ -6,15 +6,29 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
+
+  const storedDarkMode = localStorage.getItem("DARK_MODE")
+  const [darkMode, setDarkMode] = useState(storedDarkMode)
+
+  useEffect(() => {localStorage.setItem("DARK_MODE", darkMode)}, [darkMode])
+  const theme = createTheme({
+      palette: {
+      mode: darkMode ? "dark" : 'light'
+    },
+  })
+  useEffect(() => {darkMode ? document.body.style = "background: black;" : document.body.style = `background: white;`}, [darkMode]);
+
   return (
-    <div className="App">
-     <Header/>
-      <Routes>
-          <Route path='/' element={<Homepage/>} />
+   <ThemeProvider theme={theme}>
+      <div className="App">
+        <Header theme={theme} darkMode={darkMode} setDarkMode={setDarkMode}/>
+          <Routes>
+            <Route path='/' element={<Homepage theme={theme}/>} />
 
-      </Routes>
+          </Routes>
 
-    </div>
+      </div>
+    </ThemeProvider> 
   );
 }
 
