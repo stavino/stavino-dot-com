a0966704099935037a08a0fc4939384d01731fa9
stavino <89891417+stavino@users.noreply.github.com>
2023-06-15 15:37:13 -0400

    added homepage content

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,13 +1,18 @@
 import Resume from "./Resume";
+import { Typography } from "@mui/material";
 
 function Homepage(){
 
+const name = 'Steve'
+
     return (
-        <>
-        
+        <Typography>
+        <h2>- Hello</h2>
+        <h1>I'm {name} Avino</h1>
+        <p>Web Developer, competitive gamer, Dog & Cat lover</p>
         <Resume />
         
-        </>
+        </Typography>
     )
 };
 
